616d3b8fa9ea760e99af11f6a59c358050192dc7
afabricio <andrefabriciodossantos@gmail.com>
2019-04-18 16:00:43 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,31 @@
+window.addEventListener('load', ()=> {
+const sounds = document.querySelectorAll('.sound');
+const pads = document.querySelectorAll('.pads div');
+const visual = document.querySelector('.visual')
+//lets get going with the sound here
+
+const colors=[
+    "lightcoral",
+    "lightgreen",
+    "lightslategray"
+]
+
+pads.forEach((pad,index)=>{
+    pad.addEventListener('click',function(){
+        sounds[index].currentTime = 0;
+        sounds[index].play();
+        createBubbles(index);
+    })
+})
+
+
+//create a function tha create bubbles
+const createBubbles = (index) => {
+    const bubble = document.createElement("div");
+    visual.appendChild(bubble);
+    bubble.style.backgroundColor = colors[index];
+    bubble.style.animation = "jump 1s ease";
+}
+
+})
+
